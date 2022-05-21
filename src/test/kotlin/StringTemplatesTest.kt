6205aea36132f6d51528c2ea8c5d77df9a7ec6b7
gornyadrian <gorny.adr@gmail.com>
2022-05-21 22:43:46 +0200

    Add tests for string templates

---
+++ src/test/kotlin/StringTemplatesTest.kt
@@ -0,0 +1,23 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class StringTemplatesTest {
+
+    @Test
+    fun `simple templates`() {
+        val num = 10
+        val str = "the number is $num"
+        assertEquals("the number is 10", str)
+    }
+
+    @Test
+    fun `expressions in templates`() {
+        val num1 = 10
+        val num2 = 20
+        val str1 = "the number is ${num1 + num2}"
+        val str2 = "${str1.replace("number", "string").replace("is", "isn't")}!"
+        assertEquals("the number is 30", str1)
+        assertEquals("the string isn't 30!", str2)
+    }
+
+}
